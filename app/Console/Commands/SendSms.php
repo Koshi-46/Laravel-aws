bbc26fa303e7ede4c22277930ff2a8be5090e549
EC2 Default User <ec2-user@ip-10-0-11-52.ap-northeast-1.compute.internal>
2023-11-08 11:18:23 +0000

    AWS SNSを使用したSMSの送信テスト

---
+++ app/Console/Commands/SendSms.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
+
+class SendSms extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:sms';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'SMS送信のテスト';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+        $sns = App::make('aws')->createClient('sns');
+
+        $sns->publish([
+            'Message' => 'テスト',
+            'PhoneNumber' => '+81803939955' // 電話番号を入れる
+        ]);
+    }
+}
